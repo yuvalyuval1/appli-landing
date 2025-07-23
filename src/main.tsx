9e63f629dc55f500b5f11ff10bf63675c01f37b6
yuval peretz <yuval080702@gmail.com>
2025-07-23 11:15:16 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
-import { SpeedInsights } from '@vercel/speed-insights/react'; // ← הוספה
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights /> {/* ← הוספה */}
+    <SpeedInsights />
   </React.StrictMode>
 );
